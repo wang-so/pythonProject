e1bf896c118debbae7a5c6ee7f8b6c7add89e7f5
wang-so <1030596413@qq.com>
2021-08-27 16:43:14 +0800

    Initial commit

---
+++ HelloWord.py
@@ -0,0 +1,23 @@
+counter = 100          # 整型变量
+miles = 1000.0       # 浮点型变量
+name = "runoob"     # 字符串
+
+print(counter)
+print(miles)
+print(name)
+#Python允许你同时为多个变量赋值
+a = b = c = 1
+print(a)
+print(b)
+print(c)
+
+# 两个整型对象 1 和 2 的分配给变量 a 和 b，字符串对象 "runoob" 分配给变量 c。
+a, b, c = 1, 2, "runoob"
+print(a)
+print(b)
+print(c)
+
+a, b, c, d = 20, 5.5, True, 4+3j
+print(type(a), type(b), type(c), type(d))
+a = 111
+isinstance(a, int)
